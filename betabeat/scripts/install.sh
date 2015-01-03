2012101be201c04b2b37976eea9fbd65eef27018
Michal Cheben <michal@cheben.sk>
2015-01-03 19:24:03 +0100

    Alpha 133

--- betabeat/scripts/install.sh
+++ betabeat/scripts/install.sh
@@ -11,11 +11,38 @@ if [ $? -ne 0 ]; then
 fi
 echo "Modyfying uhttp configuration"
 mkdir /root/backup
-mkdir /mnt/sda1/www
-cp /etc/config/uhttpd /root/uhttpd.old
+cp /etc/config/uhttpd /root/backup/uhttpd.old
+
+if [ -d /mnt/sda1 ]; then
+	$DIR = /mnt/sda1
+else
+	if [ -d /mnt/sdb1 ]; then
+		$DIR = /mnt/sdb1
+	else
+		echo "Please mount USB or SD card!"
+		exit 1
+	fi
+fi
+
+echo "Using $DIR as storage path."
+ 
+mkdir $DIR/www
+cd $DIR/www
+mkdir cgi-bin
+cd cgi-bin
+echo "echo \"Content-type: application/json\"
+	echo \"\"
+	cat $DIR/status.xml" > status
+echo "echo \"Content-type: application/json\"
+	echo \"\"
+	cat $DIR/owl.xml" > owl
+chmod +x status
+chmod +x owl
+
 echo "config uhttpd secondary
         list listen_http        0.0.0.0:8180
         option home             /mnt/sda1/www
+        option cgi_prefix		/cgi-bin
         option max_requests     2
         option script_timeout   10
         option network_timeout  10
@@ -27,7 +54,7 @@ echo "#!/bin/ash
 if [ \$# -lt 2 ]; then
 	echo \"Please run with thermeq3 name and path as arguments.\"
 	echo \"Example:\"
-	echo \"cs3 /mnt/sda1 boiler\"
+	echo \"cs3 $DIR1 boiler\"
 else
 	echo \"Using \$2 as thermeq3 name and \$1 as path\"
 	echo \"tail -n 50 \$1/\$2.log\" > /root/ct
@@ -41,5 +68,5 @@ fi
 	" > /root/cs3.sh
 chmod +x /root/cs3.sh
 echo "Installing with $1 device name"
-/root/cs3.sh /mnt/sda1 $1
+/root/cs3.sh $DIR $1
  
