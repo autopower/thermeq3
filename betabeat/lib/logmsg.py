6f9180c9c71e2ca47592a48d5628c3bc7494832f
Michal Cheben <michal@cheben.sk>
2015-12-06 11:05:49 +0100

    L1

---
+++ betabeat/lib/logmsg.py
@@ -0,0 +1,56 @@
+import os
+import logging
+import logging.handlers
+
+log_messages = []
+logs = ['I', 'D', 'E']
+logger = None
+
+
+def update(message, log='D'):
+    """ update list with message and log level """
+    global log_messages, logs
+    """ updates log message list """
+    if log in logs:
+        log_messages.append([log, message])
+    else:
+        log_messages.append(['E', message])
+    flush()
+
+
+def flush():
+    """ flush log messages """
+    global log_messages, logger
+    # if list is not empty
+    if log_messages:
+        for k in log_messages:
+            if k[0] == 'E':
+                logger.error(k[1])
+            elif k[0] == 'D':
+                logger.debug(k[1])
+            elif k[0] == 'I':
+                logger.info(k[1])
+            else:
+                logger.error("Wrong level: " + str(k[0]) + " message: " + str(k[1]))
+        log_messages = []
+
+
+def start(log_filename):
+    """ opens log file """
+    global logger
+    logger = logging.getLogger("thermeq3")
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.handlers.TimedRotatingFileHandler(log_filename, when="W0", interval=4, backupCount=12)
+    fh.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y/%m/%d %H:%M:%S")
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    logger.info("Started with PID=" + str(os.getpid()))
+
+
+def stop():
+    global logger
+    # logger.close()
