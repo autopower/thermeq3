0e0cbef17b12c4fb0ec76118d42cdb6a22c8e1b1
Michal Cheben <michal@cheben.sk>
2015-12-10 00:04:32 +0100

    Install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,48 @@
 #!/bin/ash
-if [ \$# -lt 1 ]; then
+if [ $# -lt 1 ]; then
 	echo "Usage: install.sh <thermeq3 device name>"
 	exit 1
-echo "opkg update & install..."
-opkg update
+fi
+
+echo "Preparing..."
+echo "opkg update"
+opkg update --verbosity=0
 if [ $? -ne 0 ]; then
 	echo "Error during opkg update: $?"
 	exit $?
 fi
-opkg install python-openssl
+
+echo "Installing zip"
+opkg install unzip --verbosity=0
+if [ $? -ne 0 ]; then
+	echo "Error during installing unzip. Error: $?"
+	exit $?
+fi
+
+mkdir /root/thermeq3-install
+echo Downloading thermeq3 app
+wget --no-check-certificate --quiet --output-document /root/thermeq3-install/thermeq3.zip https://github.com/autopower/thermeq3/raw/master/scripts/thermeq3.zip
+if [ $? -ne 0 ]; then
+	echo "Error during downloading thermeq3 app: $?"
+	exit $?
+fi
+mkdir /root/thermeq3
+echo "Unzipping app"
+unzip -q /root/thermeq3-install/thermeq3.zip -d /root/thermeq3
+if [ $? -ne 0 ]; then
+	echo "Error during unzipping thermeq3 app: $?"
+	exit $?
+fi
+rm -rf /root/thermeq3-install/*
+rm -rf /root/thermeq3-install 
+
+echo "Installing libraries"
+opkg install python-openssl --verbosity=0
 if [ $? -ne 0 ]; then
 	echo "Error during installing openssl library. Error: $?"
 	exit $?
 fi
-opkg install python-expat
+opkg install python-expat --verbosity=0 
 if [ $? -ne 0 ]; then
 	echo "Error during installing expat library. Error: $?"
 	exit $?
@@ -31,14 +60,28 @@ else
 fi
 echo "Using $DIR as storage path."
 
-echo "Backing up configuration..."
-mkdir /root/backup
-cp /etc/config/uhttpd /root/backup/uhttpd.old
+if ! grep -q "0.0.0.0:8180" /etc/config/uhttpd; then
+	echo "Backing up uhttpd configuration..."
+	mkdir /root/backup
+	cp /etc/config/uhttpd /root/backup/uhttpd.old
+	echo "Modifying uhttpd configuration..."
+	echo "config uhttpd secondary
+	        list listen_http        0.0.0.0:8180
+	        option home             $DIR/www
+	        option cgi_prefix		/cgi-bin
+	        option max_requests     2
+	        option script_timeout   10
+	        option network_timeout  10
+	        option tcp_keepalive    1
+	" >> /etc/config/uhttpd
+	echo "Restarting uhttpd..."
+	/etc/init.d/uhttpd restart
+fi
 
 echo "Creating directories and cgi scripts" 
-mkdir $DIR/www
+mkdir -p $DIR/www
 cd $DIR/www
-mkdir cgi-bin
+mkdir -p cgi-bin
 cd cgi-bin
 
 echo "#!/bin/sh
@@ -57,36 +100,11 @@ chmod +x status
 chmod +x owl
 chmod +x nice
 
-echo "Modifying uhttp configuration..."
-echo "config uhttpd secondary
-        list listen_http        0.0.0.0:8180
-        option home             $DIR/www
-        option cgi_prefix		/cgi-bin
-        option max_requests     2
-        option script_timeout   10
-        option network_timeout  10
-        option tcp_keepalive    1
-" >> /etc/config/uhttpd
-echo "Restarting uhttpd..."
-/etc/init.d/uhttpd restart
-
-echo "#!/bin/ash
-if [ \$# -lt 2 ]; then
-	echo \"Please run with thermeq3 name and path as arguments.\"
-	echo \"Example:\"
-	echo \"cs3 $DIR boiler\"
-else
-	echo \"Using \$2 as thermeq3 name and \$1 as path\"
-	echo \"tail -n 50 \$1/\$2.log\" > /root/ct
-	echo \"cat \$1/\$2_error.log\" > /root/err
-	echo \"ps|grep python\" > /root/psg
-	chmod +x /root/ct
-	chmod +x /root/err
-	chmod +x /root/psg
-	ln -s \$1 sdcard
-fi
-	" > /root/cs3.sh
-chmod +x /root/cs3.sh
-echo "Installing with $1 device name"
-/root/cs3.sh $DIR $2
+echo "Installing scripts with $1 as device name and $DIR as target directory"
+echo "tail -n 50 $DIR/$1.log" > /root/ct
+echo "cat $DIR/$1_error.log" > /root/err
+echo "ps|grep python" > /root/psg
+chmod +x /root/ct
+chmod +x /root/err
+chmod +x /root/psg
  
