e9ce81a540b38a01f3fbd73a57b88d8f61b5106b
Michal Cheben <autopower@users.noreply.github.com>
2016-10-04 14:17:44 +0200

    Update config.py

--- install/beta/config.py
+++ install/beta/config.py
@@ -1,13 +1,14 @@
 # Reported name of this thermeq3 installation
-self.devname = None # ie. "thermeq3"
+# please use strings formatting!!! where neeeded
+self.setup.devname = None # ie. "thermeq3"
 # MAX Cube
-self.max_ip = None # ie. "192.168.0.10"
+self.setup.max_ip = None # ie. "192.168.0.10"
 # e-mail
-self.fromaddr = None # ie. "devices@foo.local"
-self.toaddr = None # ie. "user@foo.local"
-self.mailserver = None # SMTP host ie. "mail.foo.local"
-self.mailport = None # SMTP port ie. 25
-self.frompwd = None # SMTP authentication password, ie. "password"
+self.setup.fromaddr = None # ie. "devices@foo.local"
+self.setup.toaddr = None # ie. "user@foo.local"
+self.setup.mailserver = None # SMTP host ie. "mail.foo.local"
+self.setup.mailport = None # SMTP port ie. 25
+self.setup.frompwd = None # SMTP authentication password, ie. "password"
 # Weather info
-self.owm_api_key = None # open weather map API key, ie "123456789"
-self.location = None # geographic location, as per Yahoo WOEID. ie. "12345"
+self.setup.owm_api_key = None # open weather map API key, ie "123456789"
+self.setup.location = None # geographic location, as per Yahoo WOEID. ie. 12345
