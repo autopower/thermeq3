477533b91667bc7eb3736ed605783c87ea0c9bf2
Michal Cheben <autopowerdevice@gmail.com>
2018-11-12 23:00:02 +0100

    diag.sh update

--- install/RPi/diag.sh
+++ install/RPi/diag.sh
@@ -5,6 +5,8 @@ if [ $# -lt 1 ]; then
 	exit 1
 fi
 
+DIAGFILE=/home/pi/thermeq3/diag.txt
+
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' telnet|grep "install ok installed")
 echo Checking for telnet: $PKG_OK
 if [ "" == "$PKG_OK" ]; then
@@ -15,20 +17,28 @@ fi
 if [ -f /home/pi/thermeq3/diag.txt ]; then
 	echo "Previous diag.txt exists! Appending to file."
 fi
-echo "--- Diag script ---------" >> /home/pi/thermeq3/diag.txt
-date >> /home/pi/thermeq3/diag.txt
-echo "--- Mount directory ---------" >> /home/pi/thermeq3/diag.txt
-ls -al /mnt >> /home/pi/thermeq3/diag.txt 
-echo "--- Mount point ---------" >> /home/pi/thermeq3/diag.txt
-mount >> /home/pi/thermeq3/diag.txt
-echo "--- i/f config ---------" >> /home/pi/thermeq3/diag.txt
-ifconfig >> /home/pi/thermeq3/diag.txt
-echo "--- Ping ---------" >> /home/pi/thermeq3/diag.txt
-ping $1 -c 4 >> /home/pi/thermeq3/diag.txt
-echo "--- Telnet to MAX!Cube ---------" >> /home/pi/thermeq3/diag.txt
-sleep 3 | telnet $1 62910 >> /home/pi/thermeq3/diag.txt
+echo "--- Diag script ---------" >> $DIAGFILE
+date >> $DIAGFILE
+echo "df..."
+echo "--- Diskfree ---------" >> $DIAGFILE
+df -h >> $DIAGFILE
+echo "mount..." 
+echo "--- Mount point ---------" >> $DIAGFILE
+mount >> $DIAGFILE
+echo "ifconfig..."
+echo "--- i/f config ---------" >> $DIAGFILE
+ifconfig >> $DIAGFILE
+echo "ping..."
+echo "--- Ping ---------" >> $DIAGFILE
+ping $1 -c 4 >> $DIAGFILE
+echo "telnet..."
+echo "--- Telnet to MAX!Cube ---------" >> $DIAGFILE
+sleep 3 | telnet $1 62910 >> $DIAGFILE
 read -p "Include thermeq3.json file? WARNING: file may contains user credentials to email service! [y/n]" yn
 case $yn in
-	[Yy]* ) echo "--- thermeq3.json ---------" >> /home/pi/thermeq3/diag.txt; cat /home/pi/thermeq3/thermeq3.json >> /home/pi/thermeq3/diag.txt; 
+	[Yy]* )
+    echo "--- thermeq3.json ---------" >> $DIAGFILE
+    cat /home/pi/thermeq3/thermeq3.json >> $DIAGFILE
+    ;; 
 esac
 echo "Please post diag.txt to autopowerdevice@gmail.com"
