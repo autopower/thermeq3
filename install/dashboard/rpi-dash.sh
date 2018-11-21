e6b35be4231981cfbc8a859bd3f8625d9b6550b3
Michal Cheben <autopowerdevice@gmail.com>
2018-11-21 10:52:53 +0100

    RPi dash install script
    minor fixes, working

--- install/dashboard/rpi-dash.sh
+++ install/dashboard/rpi-dash.sh
@@ -22,17 +22,25 @@ fi
 echo "Creating apache configuration..."
 sudo chown -R pi:www-data /var/www/html/
 sudo chmod -R 770 /var/www/html/
+sudo a2enmod cgi 
+sudo service apache2 restart
  
 echo ""
 echo "Installing bootstrap"
 echo " - downloading bootstrap"
 wget --no-check-certificate --quiet --output-document $WWWDIR/bootstrap.zip https://github.com/twbs/bootstrap/releases/download/v3.3.7/bootstrap-3.3.7-dist.zip
-echo " - unzipping bootstrap"
-unzip $WWWDIR/bootstrap.zip -d $WWWDIR/
 if [ $? -ne 0 ]; then
-	echo "Error during unzipping thermeq3 app: $?"
+	echo "Error during downloading dashboard: $?"
 	exit $?
 fi
+
+echo " - unzipping bootstrap"
+unzip -q $WWWDIR/bootstrap.zip -d $WWWDIR/
+if [ $? -ne 0 ]; then
+	echo "Error during unzipping dashboard: $?"
+	exit $?
+fi
+
 echo " - moving bootstrap..."
 cp -R $WWWDIR/bootstrap-3.3.7-dist/* $WWWDIR/
 echo " - removing zip file"
@@ -50,7 +58,21 @@ fi
 echo "Creating dash file"
 echo "#!/bin/sh
 /usr/bin/env python $CGIDIR/dashboard.py" > ~/dash
+echo " - chmod +x"
 sudo chmod +x ~/dash
-sudo mv ~/dash $CGIDIR/dash
+echo " - move file"
+sudo mv ~/dash $CGIDIR/dash.sh
+echo " - replace ip:port string"
+sudo sed -i -e 's/localhost:8180/localhost/g' /usr/lib/cgi-bin/dashboard.py
 
-echo "Dashboard succesfully installed. Before browsing to http://rpi.ip/cgi-bin/dash please edit credentials in dashboard.py file"
+echo "Modyfying apache config files"
+echo ""
+if ! grep -Fq 'AddHandler cgi-script .sh' /etc/apache2/conf-available/serve-cgi-bin.conf ; then
+  echo " - add .sh handler in cgi-bin"
+  sudo sed -i '/<Directory "\/usr\/lib\/cgi-bin">/a AddHandler cgi-script .sh' /etc/apache2/conf-available/serve-cgi-bin.conf
+fi
+if ! grep -Fq '#Include conf-available\/serve-cgi-bin.conf' /etc/apache2/sites-available/000-default.conf ; then
+  echo " - include serve cgi-bin config file"
+  sudo sed -i 's/#Include conf-available\/serve-cgi-bin.conf/Include conf-available\/serve-cgi-bin.conf/g' /etc/apache2/sites-available/000-default.conf
+fi
+echo "Dashboard succesfully installed."
